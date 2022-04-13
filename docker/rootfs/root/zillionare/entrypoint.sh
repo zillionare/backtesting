8e05be18080fa52d8f57f02d1017da50caa1ac16
aaron yang <aaron_yang@jieyu.ai>
2022-04-13 12:57:59 +0800

    ✨ feat:

--- docker/rootfs/root/zillionare/entrypoint.sh
+++ docker/rootfs/root/zillionare/entrypoint.sh
@@ -5,6 +5,7 @@ if [ ! -f /config/defaults.yaml ]; then
     return
 fi
 
+pip3 install --pre zillionare-backtest --default-timeout=300 -i https://pypi.org/simple
 echo "port passed through envar: $PORT" > /var/log/backtest/backtest.log
 
 cp /config/defaults.yaml ~/zillionare/backtest/config/defaults.yaml
