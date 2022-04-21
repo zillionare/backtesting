3f7bea874b51c6abb258ae5d1fe120553c101462
aaron yang <aaron_yang@jieyu.ai>
2022-04-21 10:13:54 +0800

    🐞 fix: unary operator expected
    when call build.sh without parameters, this message raise

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,11 @@ echo $version
 docker rmi backtest
 docker build . -t backtest
 
+if [ $# -lt 1 ]; then
+    exit 0
+fi
+
 if [ $1 == "publish" ]; then
-  docker tag backtest zillionare/backtest:$version
+  docker tag backtest "zillionare/backtest:$version"
   docker push zillionare/backtest:$version
 fi
