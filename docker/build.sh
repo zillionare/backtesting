22516ae4c221f9ade8ef68f30fc3c52b10a41b1d
aaron yang <aaron_yang@jieyu.ai>
2023-05-05 19:03:48 +0800

    🐳 chore: fixed build script

--- docker/build.sh
+++ docker/build.sh
@@ -5,11 +5,11 @@ echo "the wheel file is $wheel"
 poetry build
 cp ../tests/data/* rootfs/root/.zillionare/backtest/data/
 cp ../dist/*$version*.whl rootfs/root/zillionare/
-docker rmi backtest
-docker build --build-arg version=$version --build-arg wheel=$wheel . -t backtest
+sudo docker rmi backtest
+sudo docker build --build-arg version=$version --build-arg wheel=$wheel . -t backtest
 
 # test the image
-docker run -d --name bt -v ~/zillionare/backtest/config:/config -p 7080:7080 backtest
+sudo docker run -d --name bt -v ~/zillionare/backtest/config:/config -p 7080:7080 backtest
 sleep 20
 response=`curl -sSf http://localhost:7080/`
 
@@ -21,8 +21,8 @@ if [[ "$response" == *"greetings"* ]]; then
 
     if [ $1 == "publish" ]; then
         echo "push image to docker hub"
-        docker tag backtest "zillionare/backtest:$version"
-        docker push zillionare/backtest:$version
+        sudo docker tag backtest "zillionare/backtest:$version"
+        sudo docker push zillionare/backtest:$version
     fi
 else
     echo "docker image failed"
--- docker/rootfs/root/zillionare/entrypoint.sh
+++ docker/rootfs/root/zillionare/entrypoint.sh
@@ -26,7 +26,7 @@ if [ ! -f /config/defaults.yaml ]; then
 fi
 
 # update backtest upon restart
-pip3 install --pre zillionare-backtest --default-timeout=300 -i https://pypi.org/simple
+pip3 install --pre zillionare-backtest --default-timeout=300 -i https://pypi.tuna.tsinghua.edu.cn/simple
 echo "port passed through envar: $PORT" > /var/log/backtest/backtest.log
 
 cp /config/defaults.yaml ~/zillionare/backtest/config/defaults.yaml
